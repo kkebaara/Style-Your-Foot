d9b50f5768585d246dd8e56b0f7e16ac807e804a
Kenneth Kebaara <kkebaara@yahoo.com>
2022-01-26 10:11:32 -0700

    cleaning up html

--- src/index.js
+++ src/index.js
@@ -2,8 +2,9 @@ const baseUrl = "http://localhost:3000/shoes"
 fetch(baseUrl)
     .then((resp) => resp.json())
     .then((json) => console.log(json));
+    
 
-    function activityMenu() {  
-        const activity = document.getElementById("activity");  
-        document.getElementById("activity").value = activity.options[activity.selectedIndex].text;  
-        }  
+function activityMenu() {  
+    const activity = document.getElementById("activity");  
+    document.getElementById("activity").value = activity.options[activity.selectedIndex].text;  
+}  
