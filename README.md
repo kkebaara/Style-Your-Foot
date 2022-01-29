363da7bdb5dc3e5c078a2ada89bf13890818f9f5
omarabbasi <omarkabbasi@outlook.com>
2022-01-28 16:25:50 -0800

    JS and Images

--- README.md
+++ README.md
@@ -1,2 +1,18 @@
 # Style-Your-Foot
-Shoe app, fitting activity, style, and weather to customers. Gives results based on expectations.
+-Shoe app, fitting activity, style, and weather to customers. 
+
+-Gives results based on expectations.
+
+# Navigation
+-Select an Activity from the first dropdown-menu.
+-Select a Weather attribute from the second dropdown-menu.
+-Finally select a Color from the third dropdown-menu.
+
+-Click the "Your Pair" button.
+
+-Congratulations, your custom selected pair of shoes will be displayed below.
+
+# Credits
+-Diego Valero
+-Kenneth Kebaara
+-Omar Abbasi
--- src/index.js
+++ src/index.js
@@ -1,40 +1,61 @@
-console.log("hello world")
-
 const baseUrl = "http://localhost:3000/shoes";
-const oneShoeUrl = "http://localhost:3000/shoes/1";
+//const oneShoeUrl = "http://localhost:3000/shoes/1";
 const shoeDiv = document.getElementById('shoe');
 
-function getUrl(param1, param2, param3){
-    fetch(baseUrl)
+function getShoe(param1, param2, param3){
+    fetch(baseUrl+`?activity=${param1}&resistance=${param2}&color=${param3}`)
     .then((resp) => resp.json())
     .then((allShoes) => {
+        console.log(allShoes);
+        function displayShoe(shoe){
+            const yourPair = document.getElementById("shoe")
+            yourPair.src = shoe.image
+        }
+
+        displayShoe(allShoes[0])
+        
+        
+    //allShoes.forEach(displayShoe)
+
+        // allShoes.forEach(() => {
+        //     const newShoe = document.getElementById("images")
+        // })
+        // //forEach(allShoes)
+        // //console.log(forEach)
+        
     })
 }
 
 //displayOneShoe on main page
-function handleDisplayShoe(oneShoe){
-    const newImage = document.createElement('img')
-    newImage.src = oneShoe.image;
-    shoeDiv.appendChild(newImage);
-} 
+
+// fetch(baseUrl)
+//   .then(function (response) {
+//     // The JSON data will arrive here
+//   })
+//   .catch(function (err) {
+//     // If an error occured, you will catch it here
+//   });
 
 const formSelector = document.querySelector('form');
-console.log(formSelector)
+// console.log(formSelector)
 
 //event handler and event listener
 formSelector.addEventListener("submit", (event) => {
     event.preventDefault();
-    console.log(event.target.Activities.value)
 
-    //copy and trying this out
-    // const actIvit = event.target.Activities.value;
-    // const weathA = event.target.weather.value;
-    // const colo = event.target.Color.value;
+    const activity = event.target.Activities.value
+    const weather = event.target.weather.value
+    const color = event.target.Color.value
 
-    // console.log(actIvit, weathA, colo)
+    getShoe(activity, weather, color);
+    
 
-    // const nameInput = document.getElementById("activ");
-    // const restaurantInput = document.getElementById("weat");
-    // const imageInput = document.getElementById("col");
-    //handleDisplayShoe(newShoe);
+    // console.log(event.target.Activities.value)
+    // console.log(event.target.weather.value)
+    // console.log(event.target.Color.value)
+    //we need to fetch and return a model and image below
+    //function getOneShoe(shoes) {
+        
+        // fetch("http://localhost:3000/shoes/1")
+        // .then((resp) => resp.json)
     });
